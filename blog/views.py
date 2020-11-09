9ff224830bb161ea0eaf119270eb7acd2bd9a396
sabyrbekov <aza10k@mail.ru>
2020-11-09 15:42:45 +0600

    Video 9/15

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Tag
 from django.views.generic import View
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import  *
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
@@ -12,9 +12,10 @@ def posts_list(request):
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
-    # def get(self, request, slug):
-    #     post = get_object_or_404(Post, slug__iexact=slug)
-    #     return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
 
 
 def tags_list(request):
@@ -22,26 +23,14 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 class TagDetail(ObjectDetailMixin, View):
-    # def get(self, request, slug):
     model = Tag
     template = 'blog/tag_detail.html'
-    #     tag = get_object_or_404(Tag, slug__iexact=slug)
-    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 
