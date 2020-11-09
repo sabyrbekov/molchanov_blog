747c74ed4ce2a9271dc84ba910fd164a77538c4a
sabyrbekov <aza10k@mail.ru>
2020-11-09 11:40:16 +0600

    Video 4/15

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    n = ['Oleg', 'Masha']
+    return render(request, 'blog/index.html', context={'names': n})
