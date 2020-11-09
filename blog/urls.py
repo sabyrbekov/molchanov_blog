218e4c94fb2c304dff7be51b9d69fc49b61aa8f3
sabyrbekov <aza10k@mail.ru>
2020-11-09 12:24:29 +0600

    Video 5/15

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import posts_list
+from .views import posts_list, post_detail
 
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
 ]
