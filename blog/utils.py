dc411264c16f5be2aebcedd1171b12826c7e8b82
sabyrbekov <aza10k@mail.ru>
2020-11-10 10:00:58 +0600

    Video 10/15

--- blog/utils.py
+++ blog/utils.py
@@ -42,7 +42,7 @@ class ObjectUpdateMixin:
 
     def post(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
-        bound_form = self.model_form(request.Post, instance=obj)
+        bound_form = self.model_form(request.POST, instance=obj)
 
         if bound_form.is_valid():
             new_obj = bound_form.save()
